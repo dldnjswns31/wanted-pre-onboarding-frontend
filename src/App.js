ee3807518668f62559d93c21353cfa60de4054b8
dldnjswns31 <dldnjswns32@gmail.com>
2022-10-14 11:55:04 +0900

    style: make pages, set react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Todo from "./pages/Todo";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
