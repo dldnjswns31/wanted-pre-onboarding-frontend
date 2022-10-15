7fdca8a1fe958e00d8228cead6ea667ccc57b2e8
dldnjswns31 <dldnjswns32@gmail.com>
2022-10-16 00:08:27 +0900

    feat: link to '/' when user doesn't have token and connect '/todo'

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Todo() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("onboarding_user_token");
+    if (!userToken) {
+      navigate("/");
+    }
+  }, []);
   return <div>todo</div>;
 }
 
